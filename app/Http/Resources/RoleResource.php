75df626c2ff3e8e8757fd6cb361e4a4c9f51b07b
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2020-04-26 03:59:20 +0200

    Add role resources

---
+++ app/Http/Resources/RoleResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
+
+class RoleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            $this->mergeWhen(Auth::user()->isAdmin(), [
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
+            ]),
+        ];
+    }
+}
