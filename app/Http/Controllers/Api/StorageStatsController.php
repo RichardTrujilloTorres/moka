38d76a00407932538942e41379d8e11b9af6f646
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2020-07-25 07:39:13 +0200

    Add storage stats generation action

--- app/Http/Controllers/Api/StorageStatsController.php
+++ app/Http/Controllers/Api/StorageStatsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Audit;
+use App\Console\Commands\GenerateStorageStats;
 use App\Exceptions\InvalidHistoryResourceException;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
@@ -12,13 +13,23 @@ use App\Http\Resources\UserResource;
 use App\Http\Resources\UserCollection;
 use App\StorageStat;
 use App\User;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Storage;
 
 class StorageStatsController extends BaseController
 {
-
     public function index()
     {
         return new StorageStatCollection(StorageStat::paginate($this->perPage()));
+    }
+
+    public function regenerate()
+    {
+        $output = Artisan::call('moka:storage-stats:generate');
+
+        return response()->json([
+            'status' => $output !== 0 ? 'failure' : 'success',
+            'message' => $output !== 0 ? 'Stats generation failed' : 'Stats generation completed.',
+        ], $output !== 0 ? 400 : 201);
     }
 }
